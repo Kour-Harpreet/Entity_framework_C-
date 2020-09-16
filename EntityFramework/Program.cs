c747d48dd70b276b31f78ab3f4dcddb6fb7ed60b
harpreet <Harpreet Kour>
2020-09-16 15:39:09 -0600

    Adding to the database;

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -4,6 +4,21 @@ using System.Linq;
 
 namespace EntityFramework
 {
+    // Step 1: Install dotnet-ef if it isn't:
+    // dotnet tool install --global dotnet-ef
+
+    // Step 2: Install the packages in the project:
+    // dotnet add package Microsoft.EntityFrameworkCore.Design
+    // dotnet add package Pomelo.EntityFrameworkCore.MySql
+
+    // Step 3: Create the models and context:
+    // dotnet ef dbcontext scaffold "server=localhost;port=3306;user=root;password=;database=DB_NAME" Pomelo.EntityFrameworkCore.MySql -c CONTEXT_NAME -o Models -f -d
+
+    // Step 4: Pluralize the property names in the context file.
+
+    // Step 5: Pluralize the virtual ICollections in the table classes (and their InverseProperty's).
+
+    // Step 6 (optional): Rename any instances of "Id" to "ID".
     class Program
     {
         static void Main(string[] args)
@@ -11,9 +26,26 @@ namespace EntityFramework
             // Initialize our database interaction (the context):
             using (CarsContext context = new CarsContext())
             {
-                Console.WriteLine(context.Cars.Count(x => x.Manufacturer.Name == "BMW"));
+                string make, model, colour;
 
-                Console.WriteLine(context.Cars.Count(x => x.Manufacturer.Name == "Mitsubishi"));
+                
+                Console.Write("Please enter a Make for your new car: ");
+                make = Console.ReadLine();
+
+                Console.Write("Please enter a Model for your new car: ");
+                model = Console.ReadLine();
+
+                Console.Write("Please enter a Colour for your new car: ");
+                colour = Console.ReadLine();
+
+                context.Add(new Car()
+                {
+                    Manufacturer = context.Manufacturers.Where(x => x.Name == make).SingleOrDefault(),
+                    Model = model,
+                    Colour = colour
+                });
+
+                context.SaveChanges();
             }
         }
     }
