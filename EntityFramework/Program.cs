68f2c2e8d9a31b4bd40d37b271b5cd63c6746a20
harpreet <Harpreet Kour>
2020-09-17 10:24:49 -0600

    Up to 5a; Completed mockups;

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -25,13 +25,17 @@ namespace EntityFramework
 
             3b) Create configuration methods.
                     3b1) Add an OnConfiguring() override that accepts a DbContextOptionsBuilder, if that option builder's "IsConfigured" property is false, declare the connection string for the database.
-                    3b2) Add an OnModelCreating override that accepts a ModelBuilder, call OnModelCreatingPartial() at the end.
+                     3a2) Create a constructor that acceps a DbContextOptions<THIS_CONTEXT> options, and calls the base constructor.
+                
                     3b3) Add a partial OnModelCreatingPartial override call at the end with no body.
 
             4) Set up skeleton column classes.
                 4a) Declare table name.
                 4b) Declare columns (names and types).
                 4c) Declare foreign keys (if applicable).
+
+             5) Modify context OnModelCreating to represent the models that you created.
+                5a) Add a modelBuild.Entity<MODEL_NAME>() call for each model that will set things like the foreign key mapping and the text encoding.
             */
         }
     }
